0a3a6f1b700d8f0aff2589b53e99f31a281e42db
Patrick Mahloy <patrick.mahloy@gmail.com>
2022-09-11 14:47:46 -0400

    Updates

---
+++ precache-manifest.8914a07217c8da87dfec06af007eed7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "448a53d553ba4c3a2f47033909474a43",
+    "url": "/ecommerce-material-ui/index.html"
+  },
+  {
+    "revision": "9c7a98caa827622998d5",
+    "url": "/ecommerce-material-ui/static/js/2.8d12c0b0.chunk.js"
+  },
+  {
+    "revision": "70af1bffd51a7281193fa8f9b7319597",
+    "url": "/ecommerce-material-ui/static/js/2.8d12c0b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a5d21f0f4486dbb6b37",
+    "url": "/ecommerce-material-ui/static/js/main.ec495366.chunk.js"
+  },
+  {
+    "revision": "563f2078e9c6520d032a",
+    "url": "/ecommerce-material-ui/static/js/runtime-main.e2d2c79c.js"
+  },
+  {
+    "revision": "d6dde74964329add1ad38d0ad6dc8f1e",
+    "url": "/ecommerce-material-ui/static/media/nft.d6dde749.png"
+  }
+]);
